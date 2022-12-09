04f046c0bf55b77e4417f55c0cb0b0d5d86394a6
TynanGuenther <tgunther701@gmail.com>
2022-12-09 15:46:25 -0600

    Day7 part 2 done

--- Day7/day_7.nim
+++ Day7/day_7.nim
@@ -20,14 +20,18 @@ proc fileSize(size:string):int=
   var parts = size.split(" ")
   return parseInt(parts[0])
 
+
+
+var homesub:seq[dir]
+var home:dir = dir(size:0, subdirs:homesub)
 proc parse(file:string):seq[dir]=
   let input = readFile(file)
   var line = input.split("\n")
   line.delete(line.len)
   var all:seq[dir]
-  var home:dir = dir(size:0, subdirs:all)
+  all.add(home)
+  var allnothome:seq[dir]
   var cd = home
-
   for j,i in line:
     
     if i == "$ cd /":
@@ -41,6 +45,7 @@ proc parse(file:string):seq[dir]=
       cd.subdirs.add(nw)
       cd = nw
       all.add(nw)
+      allnothome.add(nw)
     elif match(i, re"^[1-9]"):
       var size = fileSize(i)
       cd.size += size
@@ -52,11 +57,21 @@ proc parse(file:string):seq[dir]=
         cd = c
     else:
       continue
-  return all
+  return allnothome
 
 proc calc(all:seq[dir]):int=
   for i in all:
     if i.size <= 100000:
       result += i.size
+proc freeSpace(all:seq[dir]):int=
+  let difSize = 70000000-home.size
+  let remSize = 30000000 - difSize
+  result = home.size
+  for i in all:
+    if i.size > remSize and result > i.size:
+      result = i.size
 
-echo calc(parse("day7_input"))
+
+echo freeSpace(parse("day7_input"))
+#echo freeSpace(parse("test_input"))
+#echo calc(parse("day7_input"))
--- Day7/test.nim
+++
@@ -1,9 +0,0 @@
-import strutils, oop_utils/standard_class,re
-
-
-
-if match("$ ls ..", re"([cd ](?:\w+))", start=2):
-  echo "fuck"
-
-  
-
