3565e1f9f2a10086edb9a7b015b1f37d7073398f
TynanGuenther <tgunther701@gmail.com>
2022-12-09 13:45:31 -0600

    Day 7 test_input works, actual input answer too high

--- Day7/day_7.nim
+++ Day7/day_7.nim
@@ -1,4 +1,4 @@
-import strutils, re, options
+import strutils, re
 
 type
   dir = ref object
@@ -11,36 +11,54 @@ proc mkdir(parent:var dir):dir=
   var sub:seq[dir]
   direct = dir(
       size:0,
-      subdirs:nil,
-      parent: parent
+      parent: parent,
+      subdirs: sub
   )
   return direct
 
+proc fileSize(size:string):int=
+  var parts = size.split(" ")
+  return parseInt(parts[0])
 
-proc parse(file:string)=
+proc parse(file:string):seq[dir]=
   let input = readFile(file)
-  #[var all:seq[dir]
-  var cd:dir
-  var home:dir = dir(size:0, subdirs:all, parent:cd)
-  cd = home]#
+  var line = input.split("\n")
+  line.delete(line.len)
+  var all:seq[dir]
+  var home:dir = dir(size:0, subdirs:all)
+  var cd = home
+  var count = 1
 
-  var highest = 0
-  var cd = 0
-  var dirs = seq[int]
-  var prev = 0
-  for i in input:
-    case i:
-      of re"^'$ cd ..'":
-        cd = prev
-      of re"^'$ cd '":
-        var dr = i.split(" ")
-        var nw = mkdir(cd,dr[dr.len-1])
-        all.add(nw)
-        cd += 1
-      of re"^'dir'":
-        cd = mkdir(cd,i)
-        cd.subdirs.add(i)
-      of re"^[1-9]":
-        cd.size += fileSize(i)
-      else:
-        continue
+  for j,i in line:
+    
+    if i == "$ cd /":
+      echo i, "1"
+      cd = home
+    elif match(i, re"ls", start=2):
+      echo i, 2
+      continue
+    elif match(i, re"\.\.$", start=5):
+      echo i,3
+      cd = cd.parent
+    elif match(i, re"([cd ](?:\w+))", start=2):
+      echo i,4
+      var nw = mkdir(cd)
+      cd.subdirs.add(nw)
+      cd = nw
+      all.add(nw)
+    elif match(i, re"^[1-9]"):
+      echo i,5
+      var size = fileSize(i)
+      cd.size += size
+      if cd.parent != nil:
+        cd.parent.size += size
+    else:
+      continue
+  return all
+
+proc calc(all:seq[dir]):int=
+  for i in all:
+    if i.size <= 100000:
+      result += i.size
+
+echo calc(parse("day7_input"))
--- Day7/test.nim
+++ Day7/test.nim
@@ -1,39 +1,9 @@
-import strutils, classes
+import strutils, oop_utils/standard_class,re
 
 
 
-class tree:
-
-  var before:tree
-  var val:int
-  var chidren:seq[tree]
-
-  method addVal(val:int)=
-    this.val += val
-
-  method addChild(child:node)=
-    this.children.add(child)
- 
+if match("$ ls ..", re"([cd ](?:\w+))", start=2):
+  echo "fuck"
 
   
 
-var jeff:seq[node]
-var l:node
-
-for i in 0..10:
-  var cd:node
-  if i < 1 or (i > 5 and i < 7):
-    var f:seq[node]
-
-    var nw = node(val: 0,  before: nod, children: f).init()
-    cd = nw
-    cd.addChild(nw)
-
-  if i == 3:
-    cd = cd.before
-
-
-  if i < 5:
-    cd.addVal(i)
-
-echo nod.children[0].val
